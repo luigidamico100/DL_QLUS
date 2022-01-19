53ad104a4ddc2918451e48eef21be5450dd6a0ce
luigidamico100 <luigidamico.1095@gmail.com>
2022-01-19 14:20:02 +0100

    Update main_training.py

--- Code/main_training.py
+++ Code/main_training.py
@@ -20,7 +20,6 @@ import os
 #%% MAIN
 if __name__ == '__main__':
 
-#%% Set up model architecture
 
     model_ft, height_size = stat_mod_ut.initialize_model(model_name, classification, num_classes, feature_extract, use_pretrained=use_pretrained)
     print(model_ft)
@@ -28,32 +27,20 @@ if __name__ == '__main__':
     params_to_update = stat_mod_ut.get_params_to_update(model_ft, feature_extract)
     optimizer_ft = optim.Adam(params_to_update, lr=lr)
     criterion, metric = config.get_problem_stuff()
-    
-    if not experiment_all_fold:
+
+    print("Output folder: {}".format(OUTFOLDER_ALLFOLD_FOLDER))
+    os.makedirs(OUTFOLDER_ALLFOLD_FOLDER, exist_ok=True)
+    for fold_test in fold_test_list:
         dataloaders_dict, _ = dataload_ut.get_mat_dataloaders_v2(classification_classes, basePath=DATASET_PATH, num_workers=num_workers, fold_test=fold_test, height_size=height_size,
                                                                                        batch_size=batch_size, mode=mode, replicate_all_classes=replicate_all_classes,
-                                                                                       target_value=not classification)
-        
-        print("Output folder: {}\t\tBe sure that it does not exist!".format(OUTFOLDER_PATH))
+                                                                                       target_value=not classification)            
+
+        outfolder_path = OUTFOLDER_ALLFOLD_FOLDER + 'exp_fold_{}/'.format(fold_test)
+        print("Outout fold folder: {}\t\tBe sure that it does not exist".format(outfolder_path))
         models, hist = stat_mod_ut.train_model(model_ft, dataloaders_dict, criterion, metric, optimizer_ft, num_epochs=num_epochs, 
                                                is_inception=(model_name=="inception"), regularization=regularization)
 
-        stat_mod_ut.plot_and_save(models, hist, out_folder=OUTFOLDER_PATH, info_text=info_text)
+        stat_mod_ut.plot_and_save(models, hist, out_folder=outfolder_path, info_text=info_text)
     
-    else:
-        print("Output folder: {}".format(OUTFOLDER_ALLFOLD_FOLDER))
-        os.makedirs(OUTFOLDER_ALLFOLD_FOLDER, exist_ok=True)
-        for fold_test in fold_test_list:
-            dataloaders_dict, _ = dataload_ut.get_mat_dataloaders_v2(classification_classes, basePath=DATASET_PATH, num_workers=num_workers, fold_test=fold_test, height_size=height_size,
-                                                                                           batch_size=batch_size, mode=mode, replicate_all_classes=replicate_all_classes,
-                                                                                           target_value=not classification)            
-    
-            outfolder_path = OUTFOLDER_ALLFOLD_FOLDER + 'exp_fold_{}/'.format(fold_test)
-            print("Outout fold folder: {}\t\tBe sure that it does not exist".format(outfolder_path))
-            models, hist = stat_mod_ut.train_model(model_ft, dataloaders_dict, criterion, metric, optimizer_ft, num_epochs=num_epochs, 
-                                                   is_inception=(model_name=="inception"), regularization=regularization)
-    
-            stat_mod_ut.plot_and_save(models, hist, out_folder=outfolder_path, info_text=info_text)
-        
 
 
